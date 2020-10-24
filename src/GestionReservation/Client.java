2c1567c42578895072722026afb713671470880e
Yue-laure <73362066+Yue-laure@users.noreply.github.com>
2020-10-25 02:57:56 +0800

    Update Client.java

--- src/GestionReservation/Client.java
+++ src/GestionReservation/Client.java
@@ -1,4 +1,17 @@
 package GestionReservation;
 
 public class Client {
+	private String nom;
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	
+	
 }
+
