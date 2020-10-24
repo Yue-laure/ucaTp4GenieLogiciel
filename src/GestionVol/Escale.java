949238c76bd07a6f0c468487822f4d6ca34a0122
Yue-laure <73362066+Yue-laure@users.noreply.github.com>
2020-10-25 02:55:31 +0800

    Update Escale.java

--- src/GestionVol/Escale.java
+++ src/GestionVol/Escale.java
@@ -6,4 +6,20 @@ public class Escale {
     private ZonedDateTime date_atterrissage;
     private ZonedDateTime date_decollage;
     private Aeroport aeroport;
+    private Vol vol;
+    
+    
+    
+	public Aeroport getAeroport() {
+		return aeroport;
+	}
+	public void setAeroport(Aeroport aeroport) {
+		this.aeroport = aeroport;
+	}
+	public Vol getVol() {
+		return vol;
+	}
+	public void setVol(Vol vol) {
+		this.vol = vol;
+	}
 }
