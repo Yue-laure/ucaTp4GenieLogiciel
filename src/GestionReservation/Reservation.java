9008e4151e3ec325e83a62fac41d5170d6f33374
Yue-laure <73362066+Yue-laure@users.noreply.github.com>
2020-10-24 21:18:52 +0800

    Update Reservation.java

--- src/GestionReservation/Reservation.java
+++ src/GestionReservation/Reservation.java
@@ -1,4 +1,14 @@
 package GestionReservation;
 
+import java.util.Date;
+
 public class Reservation {
+	private String numero;
+	private Date date;
+	public void confirmer() {
+		
+	}
+	public void annuler() {
+		
+	}
 }
