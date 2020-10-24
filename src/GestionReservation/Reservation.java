8a220825734cad6caf52e317240f27308abd516b
Yue-laure <73362066+Yue-laure@users.noreply.github.com>
2020-10-25 05:00:34 +0800

    Update Reservation.java

--- src/GestionReservation/Reservation.java
+++ src/GestionReservation/Reservation.java
@@ -1,14 +1,27 @@
 package GestionReservation;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class Reservation {
-	private String numero;
+	private int numero;
 	private Date date;
-	public void confirmer() {
-		
-	}
-	public void annuler() {
-		
-	}
-}
+	private Vol vol; 
+	private boolean etat;
+	
+	public Reservation(int n,Vol v)
+	 { date=Calendar.getInstance().getTime();
+	   numero=n;
+	   vol=v;
+	   etat=false;
+	} 
+	public void confirmer()
+	 {etat=true;}
+	
+	 void annuler() {	 
+	 }
+	  
+	} 
+
+
+
