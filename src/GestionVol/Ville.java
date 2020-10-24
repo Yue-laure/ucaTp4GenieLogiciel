eb72aacf9c8d29ca4fd656f3e88d2d2b98b111b7
Yue-laure <73362066+Yue-laure@users.noreply.github.com>
2020-10-25 02:56:02 +0800

    Update Ville.java

--- src/GestionVol/Ville.java
+++ src/GestionVol/Ville.java
@@ -2,5 +2,14 @@ package GestionVol;
 
 public class Ville {
     private String nom;
+    private Aeroport aeroport;
+    
+    
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
 
 }
